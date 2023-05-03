af60467aedd0c23110d3a420724e26ac973bad81
Kozin Artem <kaf-artem@yandex.ru>
2023-05-03 09:11:35 +0300

    Improver decorator function time_use

--- utils/timeuse.py
+++ utils/timeuse.py
@@ -1,16 +1,26 @@
-'''Функция для вывода времени сортировки массива.'''
-import time
-from datetime import timedelta
+'''Функция-декоратор для измерения времени работы сортировок массива.'''
+from time import perf_counter
+from typing import Tuple
+
+
+def timedelta_to_msm(duration: float) -> Tuple[int, int, float]:
+    minutes = int(duration // 60)
+    seconds = int((duration % 60) // 1)
+    milisec = (duration % 1)
+    return minutes, seconds, milisec
 
 
 def time_use(func):
     def wrapper(*args, **kwargs):
-        s_time = time.monotonic()
+        start_time = perf_counter()
         result = func(*args, **kwargs)
-        e_time = time.monotonic()
+        execution_time = perf_counter() - start_time
         print(
-            f'Время сортировки массива {func.__name__}:'
-            f'{timedelta(e_time - s_time)}'
+            f'Время сортировкой {func.__name__}: {execution_time}',
+            'Минут: {} секунд: {} доля секунды: {}'.format(
+                *timedelta_to_msm(execution_time)
+            ),
+            sep='\n'
         )
         return result
     return wrapper
